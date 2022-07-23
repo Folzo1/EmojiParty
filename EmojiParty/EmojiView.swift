969a5e97adfbccf99dd6153405d33538350e17ae
Folzo1 <chrisd1231@gmail.com>
2022-07-23 09:58:11 +0800

    new stuff

--- EmojiParty/EmojiView.swift
+++ EmojiParty/EmojiView.swift
@@ -10,13 +10,23 @@ import SwiftUI
 struct EmojiView: View {
     
     var emojiSet: EmojiSet
+    @State var selectedEmojis: [String] = []
     
     var body: some View {
         
         LazyVGrid(columns: [GridItem(.adaptive(minimum: 100))]) {
+            
             ForEach(emojiSet.emojis, id: \.self) { emoji in
-                Text(emoji)
-                    .font(.system(size: 100))
+                
+                Button {
+                    
+                    selectedEmojis.append(emoji)
+                    
+                } label: {
+                    
+                    Text(emoji)
+                }
+                .font(.system(size: 100))
             }
         }
     }
