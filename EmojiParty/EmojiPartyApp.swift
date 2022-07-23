6e6ee6a3934ec54e91a152f55850eb6cd45e4f36
Folzo1 <chrisd1231@gmail.com>
2022-07-23 09:34:19 +0800

    Initial Commit

---
+++ EmojiParty/EmojiPartyApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiPartyApp.swift
+//  EmojiParty
+//
+//  Created by T Krobot on 23/7/22.
+//
+
+import SwiftUI
+
+@main
+struct EmojiPartyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
